48d1190be418786a58e5deaadd342534b31d7abd
Saptarshi Basu <sapb2004@gmail.com>
2020-09-11 18:18:20 +0530

    updated

--- lab/script/cni_deployment.sh
+++ lab/script/cni_deployment.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Setting up a webserver for testing
 sudo apt upgrade -y
+
+# Installing CNI plugin
+sudo wget https://raw.githubusercontent.com/Azure/azure-container-networking/v1.1.7/scripts/install-cni-plugin.sh
+sudo chmod +x install-cni-plugin.sh
+sudo ./install-cni-plugin.sh v1.1.7 v0.8.7
+
+# Setting up a webserver for testing
 sudo apt install apache2 -y
 sudo ufw allow 'Apache'
 cd /var/www/html
-echo "<html><h1>Hello AWS Study - Welcome To My Webpage</h1><body>`hostnamectl`</body></html>" > index.html
-
-# Installing CNI plugin
-sudo apt upgrade -y
-curl https://raw.githubusercontent.com/Azure/azure-container-networking/v1.1.7/scripts/install-cni-plugin.sh > install-cni-plugin.sh
-chmod +x install-cni-plugin.sh
-sudo ./install-cni-plugin.sh v1.1.7 v0.8.7
+sudo echo "<html><h1>Hello AWS Study - Welcome To My Webpage</h1><body>`hostnamectl`</body></html>" > index.html
